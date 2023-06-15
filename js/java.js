d13cc4a49b7d6b47ebffd8343ced7786a303c523
zebrito <133021015+zebrito@users.noreply.github.com>
2023-06-14 23:42:08 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,14 @@
+
+
+function cambiarback() {
+  document.body.style.backgroundColor = "black";
+  document.getElementById("head").style.backgroundColor = "black";
+  document.getElementById("ing").style.backgroundColor = "black";
+  document.getElementById("prep").style.backgroundColor = "black";
+  document.getElementById("ing").style.color = "white";
+    document.getElementById("tit").style.color = "white";
+}
+
+function webmaster() {
+  document.getElementById("eze").innerHTML = "Ezequiel Brancaleone 2023";
+}
